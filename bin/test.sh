66a0a9e5f75b013f224a937834c09242b228e479
Nathan Miller <nmiller217@gmail.com>
2019-05-30 07:07:18 -0700

    add deployment and test bin scripts

--- bin/test.sh
+++ bin/test.sh
@@ -7,11 +7,12 @@ while getopts ":dp" opt; do
     case "$opt" in
         d)
           DEBUG=true
-          docker-compose run --name test-api --entrypoint /code/bin/test-entrypoint.sh   --rm 
+          #docker-compose run --name test-api --entrypoint /code/bin/test-entrypoint.sh --rm 
+          docker-compose run --entrypoint /code/bin/test-entrypoint.sh api
           ;;
         p)
           DEBUG=false
-          docker-compose up
+          docker-compose run --entrypoint /code/bin/test-entrypoint.sh api
           ;;
         *)
           usage
