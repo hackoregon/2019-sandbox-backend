cfec16e12da34e14d7752a9b9c317e431b801a1d
Nathan Miller <nathan.miller@hackoregon.org>
2019-08-19 17:06:06 -0700

    add encrypted credentials

--- hackoregon_sandbox/api/google_sheets.py
+++ hackoregon_sandbox/api/google_sheets.py
@@ -2,7 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-CREDENTIALS_FILE = "src_files/FormTestProject-3c43acfaeacd.json"
+CREDENTIALS_FILE = "src_files/credentials.json"
 #CREDENTIALS_FILE = "FormTestProject-3c43acfaeacd.json"
 SCOPE = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/drive']
 CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, SCOPE)
