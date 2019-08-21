4da6d0e3daae63aba9216eccd5a5dfe603894168
Nathan Miller <nathan.miller@hackoregon.org>
2019-08-21 11:03:03 -0700

    remove endpoints

--- hackoregon_sandbox/api/urls.py
+++ hackoregon_sandbox/api/urls.py
@@ -1,20 +1,20 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from api import views
+#from api import views
                        
 
 router = DefaultRouter()
-router.register(r'Tags', views.TagViewSet)
-router.register(r'Layers', views.LayerViewSet)
-router.register(r'Packages', views.PackageViewSet)
-router.register(r'PdxMsa2010CensusBlockGroups', views.PdxMsa2010CensusBlockGroupsViewSet)
-router.register(r'PdxMsa2010CensusTracts', views.PdxMsa2010CensusTractsViewSet)
-router.register(r'PdxMsaNcdbs', views.PdxMsaNcdbViewSet)
+# router.register(r'Tags', views.TagViewSet)
+# router.register(r'Layers', views.LayerViewSet)
+# router.register(r'Packages', views.PackageViewSet)
+# router.register(r'PdxMsa2010CensusBlockGroups', views.PdxMsa2010CensusBlockGroupsViewSet)
+# router.register(r'PdxMsa2010CensusTracts', views.PdxMsa2010CensusTractsViewSet)
+# router.register(r'PdxMsaNcdbs', views.PdxMsaNcdbViewSet)
 
 
 urlpatterns = [
-    url(r'^create_layer/', views.create_layer, name='create_layer'),
-    url(r'^create_package/', views.create_package, name='create_package'),
+    #url(r'^create_layer/', views.create_layer, name='create_layer'),
+    #url(r'^create_package/', views.create_package, name='create_package'),
     url(r'^', include(router.urls)),        
 ]
