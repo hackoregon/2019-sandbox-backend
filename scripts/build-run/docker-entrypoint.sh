ef33d53b9f60b57adfe91a4eb6fdb812e46e320f
Nathan Miller <nathan.miller@hackoregon.org>
2019-09-05 21:38:28 -0700

    refactor docker-entrypoint.sh for readability

--- scripts/build-run/docker-entrypoint.sh
+++ scripts/build-run/docker-entrypoint.sh
@@ -1,21 +1,27 @@
 #! /bin/bash
 
-# wait-for-postgres.sh
-# https://docs.docker.com/compose/startup-order/
-
-# http://linuxcommand.org/lc3_man_pages/seth.html:
-# -e  Exit immediately if a command exits with a non-zero status.
+# -e  Exit immediately if a command exits with a non-zero status:
+# (http://linuxcommand.org/lc3_man_pages/seth.html)
 set -e
+
+if [ -z ${DEBUG+x} ]; then 
+  echo "DEBUG var is unset, setting to False" && export DEBUG=false ; 
+else 
+  echo "var is set to '$DEBUG'";
+fi
+
+echo Debug: $DEBUG
+
+if [ -z ${TRAVIS+x} ]; then 
+  echo "TRAVIS var is unset, setting to False" && export TRAVIS=false ; 
+else 
+  echo "var is set to '$TRAVIS'";
+fi
+
+echo Travis: $TRAVIS
 
 # Pull in environment variables values from AWS Parameter Store, and preserve the exports
 # source usage per https://stackoverflow.com/q/14742358/452120 (iff running on travis-ci)
-if [ -z ${DEBUG+x} ]; then echo "DEBUG var is unset, setting to False" && export DEBUG=false ; else echo "var is set to '$DEBUG'"; fi
-
-if [ -z ${TRAVIS+x} ]; then echo "TRAVIS var is unset, setting to False" && export TRAVIS=false ; else echo "var is set to '$TRAVIS'"; fi
-
-echo Debug: $DEBUG
-echo Travis: $TRAVIS
-
 if [ $DEBUG = "false" ] && [ $TRAVIS = "false" ]; then
   source /code/src_files/scripts/deploy/get-ssm-parameters.sh
 fi
@@ -24,15 +30,15 @@ fi
 
 chmod +x *.py
 
-echo "Make migrations"
-python -Wall manage.py makemigrations api
+#echo "Make migrations"
+#python -Wall manage.py makemigrations api
 
-echo "Migrate"
-python -Wall manage.py migrate
+echo "Migrate..."
+python -Wall manage.py migrate --noinput api
 
 # Collect static files
-echo "Collect static files"
+echo "Collect static files..."
 python -Wall manage.py collectstatic --noinput
 
 echo "Run server..."
-gunicorn backend.wsgi:application -c gunicorn_conf.py
+gunicorn backend.wsgi -c gunicorn_conf.py
